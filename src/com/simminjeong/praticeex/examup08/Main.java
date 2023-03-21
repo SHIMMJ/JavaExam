e04f92f0424acaefcc617d0b86784653f325b92f
simminjeong <pooh5045@naver.com>
2023-03-21 16:32:29 +0900

    네트워크 통신 연습문제

--- src/com/simminjeong/praticeex/examup08/Main.java
+++ src/com/simminjeong/praticeex/examup08/Main.java
@@ -1,6 +1,7 @@
 package com.simminjeong.praticeex.examup08;
 
 import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
@@ -9,6 +10,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedInputStream in = new BufferedInputStream(new URL("https://alimipro.com/favicon.ico").openStream());
+        FileOutputStream fileOutputStream = new FileOutputStream("output.ico");{
+            
+            byte dataBuffer[] = new byte[1024];
+            int byteRead;
+            
+            while((byteRead = in.read(dataBuffer,0,1024))!=-1) {
+                fileOutputStream.write(dataBuffer,0,byteRead);
+            }
+        }
+        
     }
 
 }
