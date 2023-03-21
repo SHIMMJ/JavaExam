e68194327c4088b9008c0599a1135e703a5244c0
simminjeong <pooh5045@naver.com>
2023-03-21 16:37:53 +0900

    Java Unit Test 연습문제 2번

---
+++ src/com/simminjeong/praticeex/examup10/DownCounter.java
@@ -0,0 +1,19 @@
+package com.simminjeong.praticeex.examup10;
+
+public class DownCounter {
+
+    private int count = 0;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void decrease() {
+        count--;
+    }
+
+}
